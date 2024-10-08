361bb0dece89b7114ff8b8281f4ad6ddfd4594f3
luke <lukeforenergy@gmail.com>
2024-10-08 11:45:37 +0800

    Aura Player Controller

--- Source/Aura_sample/Private/Player/AuraPlayerController.cpp
+++ Source/Aura_sample/Private/Player/AuraPlayerController.cpp
@@ -3,3 +3,29 @@
 
 #include "Player/AuraPlayerController.h"
 
+#include "EnhancedInputSubsystems.h"
+
+AAuraPlayerController::AAuraPlayerController()
+{
+	// 當server更新值，發送到client端機制
+	bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	// 避免AuraContext尚未指定，未指定時會回傳false並中斷
+	check(AuraContext);
+
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+	Subsystem->AddMappingContext(AuraContext, 0);
+
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+}
--- Source/Aura_sample/Public/Player/AuraPlayerController.h
+++ Source/Aura_sample/Public/Player/AuraPlayerController.h
@@ -6,6 +6,9 @@
 #include "GameFramework/PlayerController.h"
 #include "AuraPlayerController.generated.h"
 
+// 要加這行需在專案名.Build.cs 檔案中的PublicDependencyModuleNames.AddRange 加上 "EnhancedInput"
+class UInputMappingContext;
+
 /**
  * 
  */
@@ -14,4 +17,13 @@ class AURA_SAMPLE_API AAuraPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
+public:
+	// 建構函式
+	AAuraPlayerController();
+protected:
+	// 覆寫BeginPlay方法
+	virtual void BeginPlay() override;
+private:
+	UPROPERTY(EditAnywhere, Category = "Input");
+	TObjectPtr<UInputMappingContext> AuraContext;
 };
