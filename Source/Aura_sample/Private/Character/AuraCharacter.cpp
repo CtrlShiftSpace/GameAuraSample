6cc1b5763722fc24a0e7fc154670c6517d9a96de
luke <lukeforenergy@gmail.com>
2024-10-10 15:49:21 +0800

    Game Mode (finished)

--- Source/Aura_sample/Private/Character/AuraCharacter.cpp
+++ Source/Aura_sample/Private/Character/AuraCharacter.cpp
@@ -3,16 +3,31 @@
 
 #include "Character/AuraCharacter.h"
 #include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
 AAuraCharacter::AAuraCharacter()
 {
 	// 建立SpringArm元件
 	CharacterSpringArm = CreateDefaultSubobject<USpringArmComponent>("CharacterSpringArm");
+	CharacterSpringArm->bInheritPitch = false;
+	CharacterSpringArm->bInheritRoll = false;
+	CharacterSpringArm->bInheritYaw = false;
 	CharacterSpringArm->SetupAttachment(GetCapsuleComponent(), FName("CharacterSpringArm"));
 
 	// 建立攝影機
 	CharacterCamera = CreateDefaultSubobject<UCameraComponent>("CharacterCamera");
 	CharacterCamera->SetupAttachment(CharacterSpringArm, FName("CharacterCamera"));
+
+	// 設定角色移動面對方向也會跟著移動
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.f, 400.f, 0.f);
+	GetCharacterMovement()->bConstrainToPlane = true;
+	GetCharacterMovement()->bSnapToPlaneAtStart = true;
+
+	// 取消預設的旋轉朝向
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationYaw = false;
 
 }
 
