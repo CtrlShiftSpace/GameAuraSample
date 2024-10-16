19342718a68d9b8d4cf4bf6e13f7ec546c565165
luke <lukeforenergy@gmail.com>
2024-10-16 16:55:38 +0800

    The Player State

---
+++ Source/Aura_sample/Private/Player/AuraPlayerState.cpp
@@ -0,0 +1,10 @@
+// Copyright Luke
+
+
+#include "Player/AuraPlayerState.h"
+
+AAuraPlayerState::AAuraPlayerState()
+{
+	// 主要用於控制對象(遊戲角色、物體)狀態的更新頻率
+	NetUpdateFrequency = 100.f;
+}
---
+++ Source/Aura_sample/Public/Player/AuraPlayerState.h
@@ -0,0 +1,21 @@
+// Copyright Luke
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "AuraPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_SAMPLE_API AAuraPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+	
+public:
+	// 建構函式
+	AAuraPlayerState();
+
+};
