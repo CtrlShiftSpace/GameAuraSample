b5059c420a706791ee76bab6af698f8800273c59
luke <lukeforenergy@gmail.com>
2024-12-03 15:46:26 +0800

    [Section 8] Player Level and Combat Interface

--- Source/Aura_sample/Public/Player/AuraPlayerState.h
+++ Source/Aura_sample/Public/Player/AuraPlayerState.h
@@ -21,8 +21,11 @@ class AURA_SAMPLE_API AAuraPlayerState : public APlayerState, public IAbilitySys
 public:
 	// 建構函式
 	AAuraPlayerState();
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
 	UAttributeSet* GetAttributeSet() const { return AttributeSet; }
+	// 使用 FORCEINLINE 可以避免頻繁調用的開銷
+	FORCEINLINE int32 GetPlayerLevel() const { return Level; }
 
 protected:
 	UPROPERTY(VisibleAnywhere)
@@ -31,4 +34,12 @@ protected:
 	UPROPERTY()
 	TObjectPtr<UAttributeSet> AttributeSet;
 
+private:
+
+	// 玩家等級
+	UPROPERTY(VisibleAnywhere, ReplicatedUsing = OnRep_Level)
+	int32 Level = 1;
+
+	UFUNCTION()
+	void OnRep_Level(int32 OldLevel);
 };
