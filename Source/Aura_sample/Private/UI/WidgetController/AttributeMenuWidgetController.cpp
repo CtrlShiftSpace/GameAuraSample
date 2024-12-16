8bc680d66cfe488c9b724b4d2100fff54bbb21bd
luke <lukeforenergy@gmail.com>
2024-12-16 10:43:36 +0800

    [Section 9] Attribute Menu Widget Controller

---
+++ Source/Aura_sample/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,14 @@
+// Copyright Luke
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+
+}
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+
+}
