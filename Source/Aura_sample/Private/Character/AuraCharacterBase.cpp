9e82c65fde363e525cd70312dc697c80068884f0
luke <lukeforenergy@gmail.com>
2025-03-05 11:51:48 +0800

    [Section 16] Melee Attack Gameplay Event

--- Source/Aura_sample/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura_sample/Private/Character/AuraCharacterBase.cpp
@@ -83,7 +83,7 @@ void AAuraCharacterBase::BeginPlay()
 	
 }
 
-FVector AAuraCharacterBase::GetCombatSocketLocation()
+FVector AAuraCharacterBase::GetCombatSocketLocation_Implementation()
 {
 	check(Weapon);
 	return Weapon->GetSocketLocation(WeaponTipSocketName);
