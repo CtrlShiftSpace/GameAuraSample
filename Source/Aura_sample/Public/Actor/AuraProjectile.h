484072fc8ed0249f9d303dc8e9e44f69200f0b5d
luke <lukeforenergy@gmail.com>
2025-07-29 11:47:10 +0800

    [Section 19] Elementalist Behavior Tree

--- Source/Aura_sample/Public/Actor/AuraProjectile.h
+++ Source/Aura_sample/Public/Actor/AuraProjectile.h
@@ -34,6 +34,9 @@ protected:
 	UFUNCTION()
 	void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<USphereComponent> Sphere;
+
 private:
 
 	// 存活在畫面中的時間
@@ -41,9 +44,6 @@ private:
 	float LifeSpan = 15.f;
 
 	bool bHit = false;
-
-	UPROPERTY(VisibleAnywhere)
-	TObjectPtr<USphereComponent> Sphere;
 
 	// 衝擊的粒子效果
 	UPROPERTY(EditAnywhere)
