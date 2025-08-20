b11f84aff315236f3158ed341ccabf5f67861049
luke <lukeforenergy@gmail.com>
2025-08-20 11:53:04 +0800

    [Section 23] Showing Attribute Points in the HUD

--- Source/Aura_sample/Private/Character/AuraCharacter.cpp
+++ Source/Aura_sample/Private/Character/AuraCharacter.cpp
@@ -130,12 +130,16 @@ void AAuraCharacter::AddToPlayerLevel_Implementation(int32 InPlayerLevel)
 
 void AAuraCharacter::AddToAttributePoints_Implementation(int32 InAttributePoints)
 {
-	// TODO: 在 PlayerState 新增 增加AttributePoints
+	AAuraPlayerState* AuraPlayerState = GetPlayerState<AAuraPlayerState>();
+	check(AuraPlayerState);
+	AuraPlayerState->AddToAttributePoints(InAttributePoints);
 }
 
 void AAuraCharacter::AddToSpellPoints_Implementation(int32 InSpellPoints)
 {
-	// TODO: 在 PlayerState 新增 增加SpellPoints
+	AAuraPlayerState* AuraPlayerState = GetPlayerState<AAuraPlayerState>();
+	check(AuraPlayerState);
+	AuraPlayerState->AddToSpellPoints(InSpellPoints);
 }
 
 int32 AAuraCharacter::GetPlayerLevel_Implementation()
