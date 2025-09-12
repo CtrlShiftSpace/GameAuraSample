a45e9c77efe09dd02a1545d0382b9ec0cedea139
luke <lukeforenergy@gmail.com>
2025-09-12 10:57:53 +0800

    [Section 26] Aura Beam Spell

---
+++ Source/Aura_sample/Public/AbilitySystem/Abilities/AuraBeamSpell.h
@@ -0,0 +1,38 @@
+// Copyright Luke
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/AuraDamageGameplayAbility.h"
+#include "AuraBeamSpell.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_SAMPLE_API UAuraBeamSpell : public UAuraDamageGameplayAbility
+{
+	GENERATED_BODY()
+public:
+	// 儲存滑鼠點擊位置與Actor
+	UFUNCTION(BlueprintCallable)
+	void StoreMouseDataInfo(const FHitResult& HitResult);
+
+	// 儲存擁有此能力的 PlayerController
+	UFUNCTION(BlueprintCallable)
+	void StoreOwnerPlayerController();
+	
+protected:
+
+	// 滑鼠點擊位置
+	UPROPERTY(BlueprintReadWrite, Category = "Beam")
+	FVector MouseHitLocation;
+
+	// 滑鼠點擊的 Actor
+	UPROPERTY(BlueprintReadWrite, Category = "Beam")
+	TObjectPtr<AActor> MouseHitActor;
+
+	// 擁有此能力的 PlayerController
+	UPROPERTY(BlueprintReadWrite, Category = "Beam")
+	TObjectPtr<APlayerController> OwnerPlayerController;
+};
