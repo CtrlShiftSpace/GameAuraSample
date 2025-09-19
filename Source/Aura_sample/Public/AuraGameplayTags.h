1ced929ca1e52fe946ea509290490ab3683e1140
luke <lukeforenergy@gmail.com>
2025-09-19 10:57:45 +0800

    [Section 26] Electrocute Cost Cooldown and Damage

--- Source/Aura_sample/Public/AuraGameplayTags.h
+++ Source/Aura_sample/Public/AuraGameplayTags.h
@@ -87,6 +87,7 @@ public:
 	FGameplayTag Abilities_Lightning_Electrocute;
 	
 	FGameplayTag Cooldown_Fire_FireBolt;
+	FGameplayTag Cooldown_Lightning_Electrocute;
 
 	FGameplayTag CombatSocket_Weapon;
 	FGameplayTag CombatSocket_RightHand;
