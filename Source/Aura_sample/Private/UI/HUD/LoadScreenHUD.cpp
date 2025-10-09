c5acf69a448a28ae62fa8b6dfe614d9de398406b
luke <lukeforenergy@gmail.com>
2025-10-09 15:57:13 +0800

    [Section 30] Constructing a View Model

--- Source/Aura_sample/Private/UI/HUD/LoadScreenHUD.cpp
+++ Source/Aura_sample/Private/UI/HUD/LoadScreenHUD.cpp
@@ -2,4 +2,19 @@
 
 
 #include "UI/HUD/LoadScreenHUD.h"
+#include "Blueprint/UserWidget.h"
+#include "UI/ViewModel/MVVM_LoadScreen.h"
+#include "UI/Widget/LoadScreenWidget.h"
 
+void ALoadScreenHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// 創建讀取畫面ViewModel元件
+	LoadScreenViewModel = NewObject<UMVVM_LoadScreen>(this, LoadScreenViewModelClass);
+
+	// 創建並添加讀取畫面UI元件
+	LoadScreenWidget = CreateWidget<ULoadScreenWidget>(GetWorld(), LoadScreenWidgetClass);
+	LoadScreenWidget->AddToViewport();
+	
+}
--- Source/Aura_sample/Public/UI/HUD/LoadScreenHUD.h
+++ Source/Aura_sample/Public/UI/HUD/LoadScreenHUD.h
@@ -6,6 +6,8 @@
 #include "GameFramework/HUD.h"
 #include "LoadScreenHUD.generated.h"
 
+class UMVVM_LoadScreen;
+class ULoadScreenWidget;
 /**
  * 
  */
@@ -14,4 +16,24 @@ class AURA_SAMPLE_API ALoadScreenHUD : public AHUD
 {
 	GENERATED_BODY()
 	
+public:
+
+	// 讀取畫面UI類別
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<UUserWidget> LoadScreenWidgetClass;
+
+	// 讀取畫面UI元件
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<ULoadScreenWidget> LoadScreenWidget;
+
+	// 讀取畫面ViewModel類別
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<UMVVM_LoadScreen> LoadScreenViewModelClass;
+
+	// 讀取畫面ViewModel元件
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<UMVVM_LoadScreen> LoadScreenViewModel;
+	
+protected:
+	virtual void BeginPlay() override;
 };
