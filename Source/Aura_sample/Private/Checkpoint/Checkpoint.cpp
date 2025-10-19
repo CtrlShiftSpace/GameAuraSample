ef7304db3ebc2bcd1b90f1970e6a419b75cdf981
luke <lukeforenergy@gmail.com>
2025-10-19 10:53:42 +0800

    [Section 31] Checkpoints

---
+++ Source/Aura_sample/Private/Checkpoint/Checkpoint.cpp
@@ -0,0 +1,57 @@
+// Copyright Luke
+
+
+#include "Checkpoint/Checkpoint.h"
+#include "Components/SphereComponent.h"
+
+ACheckpoint::ACheckpoint(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+	PrimaryActorTick.bCanEverTick = false;
+	
+	// 建立 Mesh
+	CheckpointMesh = CreateDefaultSubobject<UStaticMeshComponent>("CheckpointMesh");
+	CheckpointMesh->SetupAttachment(GetRootComponent());
+	CheckpointMesh->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	CheckpointMesh->SetCollisionResponseToAllChannels(ECR_Block);
+
+	Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
+	// Sphere 放置在 CheckpointMesh 下
+	Sphere->SetupAttachment(CheckpointMesh);
+	// 啟用碰撞，但僅用查詢，所以物體不會阻擋，可用於重疊檢測
+	Sphere->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	// 預設所有碰撞channel 為忽略
+	Sphere->SetCollisionResponseToAllChannels(ECR_Ignore);
+	// 角色會觸發重疊事件
+	Sphere->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+}
+
+void ACheckpoint::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	// 如果偵測到進入的是玩家
+	if (OtherActor->ActorHasTag(FName("Player")))
+	{
+		// 處理 Checkpoint 物件發光效果
+		HandleGlowEffects();
+	}
+}
+
+void ACheckpoint::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// 綁定重疊事件
+	Sphere->OnComponentBeginOverlap.AddDynamic(this, &ACheckpoint::OnSphereOverlap);
+	
+}
+
+void ACheckpoint::HandleGlowEffects()
+{
+	// 設定取消碰撞
+	Sphere->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	// 建立動態 MaterialInstance
+	UMaterialInstanceDynamic* DynamicMaterialInstance = UMaterialInstanceDynamic::Create(CheckpointMesh->GetMaterial(0), this);
+	// 設定動態建立的 MaterialInstance
+	CheckpointMesh->SetMaterial(0, DynamicMaterialInstance);
+	// 呼叫 CheckpointReached 事件進行存檔
+	CheckpointReached(DynamicMaterialInstance);
+}
---
+++ Source/Aura_sample/Public/Checkpoint/Checkpoint.h
@@ -0,0 +1,43 @@
+// Copyright Luke
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerStart.h"
+#include "Checkpoint.generated.h"
+
+class USphereComponent;
+/**
+ * 
+ */
+UCLASS()
+class AURA_SAMPLE_API ACheckpoint : public APlayerStart
+{
+	GENERATED_BODY()
+
+public:
+	ACheckpoint(const FObjectInitializer& ObjectInitializer);
+
+	// 接觸到其他物體的重疊函式
+	UFUNCTION()
+	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	virtual void BeginPlay() override;
+
+	// 玩家到達紀錄點時的事件
+	UFUNCTION(BlueprintImplementableEvent)
+	void CheckpointReached(UMaterialInstanceDynamic* DynamicMaterialInstance);
+
+	// 處理發光效果的函式
+	void HandleGlowEffects();
+	
+private:
+	// 用來顯示紀錄點位置的 Mesh
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UStaticMeshComponent> CheckpointMesh;
+
+	// 用來偵測接觸狀況的碰撞體
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USphereComponent> Sphere;
+	
+};
